4efd4d57a8f42e6b0eca06f7f380679d0a39db02
lisshporta <lis.shporta@gmail.com>
2026-04-22 23:29:30 +0200

    test

---
+++ frontend/functions/sanctum/[[path]].ts
@@ -0,0 +1,13 @@
+export const onRequest: PagesFunction = async ({ request, params }) => {
+  const url = new URL(request.url);
+  const backendUrl = `https://factum-5v5v.onrender.com/sanctum/${params.path || ''}${url.search}`;
+  const backendRequest = new Request(backendUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.text() : undefined,
+    redirect: 'manual',
+    credentials: 'include',
+  });
+  const response = await fetch(backendRequest);
+  return response;
+};
